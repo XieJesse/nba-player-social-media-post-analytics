24dee389f35cc9599e6049982f408995c10e553e
JesseStuy20 <jxie20@stuy.edu>
2025-12-10 11:25:18 -0500

    fix to report error handling

--- streamlit_tool/report.py
+++ streamlit_tool/report.py
@@ -6,4 +6,4 @@ pdf_file_path = "./report.pdf"
 if os.path.exists(pdf_file_path):
     st.pdf(pdf_file_path)
 else:
-    st.error(f"Path not found", pdf_file_path)
+    st.error(f"Path not found"+pdf_file_path)
