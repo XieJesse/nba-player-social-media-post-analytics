ea63a9c4a7f805e4e042d3e8c1efd8d52f8384e5
JesseStuy20 <jxie20@stuy.edu>
2025-12-10 11:35:56 -0500

    fixed issue with report display

--- streamlit_tool/report.py
+++ streamlit_tool/report.py
@@ -7,7 +7,9 @@ if os.path.exists(pdf_file_path):
     st.pdf(pdf_file_path)
 elif (os.path.exists("../report.pdf")):
     st.pdf("../report.pdf")
+elif (os.path.exists("/mount/src/nba-player-social-media-post-analytics/streamlit_tool/report.pdf")):
+    st.pdf("/mount/src/nba-player-social-media-post-analytics/streamlit_tool/report.pdf")
 else:
     st.error(f"Path not found: "+pdf_file_path)
-    # current_file_path = os.path.abspath(__file__)
-    # st.write(f"{current_file_path}")
+    current_file_path = os.path.abspath(__file__)
+    st.write(f"{current_file_path}")
